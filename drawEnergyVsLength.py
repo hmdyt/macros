4f1de83a23614e27291f180ddf87c665c1195c9a
hmdyt <konnyaku108@gmail.com>
2023-02-14 17:42:28 +0900

    update lib

--- drawEnergyVsLength.py
+++ drawEnergyVsLength.py
@@ -1,18 +1,20 @@
+# エネルギーvs飛跡長をplotする
+# Usage:
+#     pwd -> /path/to/perXXXX
+#     python3 drawEnergyVsLength.py
+
 import ROOT as r
 from lib.file import CvvarReader
 from lib.settings import set_batch
 
-ENERGY_RANGE = (0, 3000)
-LENGTH_RANGE = (0, 50)
+ENERGY_RANGE = (0, 100)
+LENGTH_RANGE = (0, 0.02)
 
 set_batch(True)
 reader = CvvarReader()
-
-hist = r.TH2D("hist", "energy vs length;energy;length",
-              100, *ENERGY_RANGE,
-              100, *LENGTH_RANGE)
-
-for e in reader.tree:
-    hist.Fill(e.ene_l, e.length)
-
-reader.save(hist, "colz", "drawEnergyVsLength.png")
+reader.hist(
+    "length:ene_l",
+    f"100, {ENERGY_RANGE[0]}, {ENERGY_RANGE[1]}, 100, {LENGTH_RANGE[0]}, {LENGTH_RANGE[1]}",
+    draw_arguments="colz",
+    logz=True
+)
--- lib/file.py
+++ lib/file.py
@@ -19,25 +19,39 @@ class CvvarReader:
         self.tfile = r.TFile.Open(CVVAR_ROOTFILE)
         self.tree = self.tfile.cvvar_tree
 
-    def save(self, object: r.TObject, opt: str, save_as: str) -> str:
+    def save(self, object: r.TObject, opt: str, save_as: str, logz=False) -> str:
         """
         objectをcanvasにdrawして保存する\n
         ex) `reader.save(hist, 'hist.png')`
         """
         self.canvas = r.TCanvas()
         object.Draw(opt)
+        if logz:
+            r.gPad.SetLogz()
+
         self.canvas.SaveAs(save_as)
 
-    def hist(self, branch: str) -> None:
+    def hist(self, branch: str, hist_arguments: str, draw_arguments: str = "", logz=False, logy=False) -> None:
         """
         あるブランチのヒストグラムを作成する\n
-        ex) `reader.hist('ene_l')`
+
+        ex) ene_lの0-1000を100binでdraw\n
+        `reader.hist('ene_l', '100, 0, 1000')`\n
+
+        ex) enel vs lentghの2d hist (0-50keV, 0-5cm) \n
+        `reader.hist('length:ene_l', '100, 0, 50, 100, 0, 5', draw_arguments='colz')`\n
         """
         id = uuid.uuid1()
-        hist_name = f'hist_{id}'
-        self.tree.Draw(f'{branch}>>{hist_name}')
-        hist = r.gROOT.FindObject(hist_name)
 
         c = r.TCanvas('canvas_{id}')
-        hist.Draw()
+        if logy:
+            r.gPad.SetLogy()
+        if logz:
+            r.gPad.SetLogz()
+
+        hist_name = f'hist_{id}'
+        self.tree.Draw(f'{branch}>>{hist_name}({hist_arguments})')
+        hist = r.gROOT.FindObject(hist_name)
+
+        hist.Draw(draw_arguments)
         c.SaveAs(f'{hist_name}.png')
